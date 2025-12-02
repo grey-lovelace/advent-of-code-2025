164b8a0c01b2e77ab5b2e2a9329eb3943eb7d960
Grey Lovelace <austin.grey.lovelace@gmail.com>
2025-12-02 00:24:35 -0600

    Refactor day02 to use regex instead

--- src/days/day02/day02.ts
+++ src/days/day02/day02.ts
@@ -3,50 +3,25 @@ import Day from "../day.ts";
 
 export default class Day02 extends Day {
   dayPath = () => import.meta.dirname!;
-  expectedPart1Results = () => [
-    ["sample.txt", 1227775554],
-  ];
-  expectedPart2Results = () => [
-    ["sample.txt", 4174379265],
-  ];
-  
-  part1 = (input: string) => {
-    const data = input
-      .matchAllAsList(/\d+\-\d+/g)
-      .map(pair => pair[0].split("-"))
-      .map(pair => range(pair[0].asNumber(), pair[1].asNumber()))
-      .flatten()
-      .unique()
-      .map(num => `${num}`)
-      .filter(num => num.length % 2 == 0)
-      .map(num => [num.slice(0, num.length/2), num.slice(num.length/2)])
-      .filter(([num1,num2]) => num1===num2)
-      .map(([num1,num2])=> Number(num1+num2))
-      .sum()
-    return data
-  }
-  
-  part2 = (input: string) => {
-    const data = input
-      .matchAllAsList(/\d+\-\d+/g)
-      .map(pair => pair[0].split("-"))
-      .map(pair => range(pair[0].asNumber(), pair[1].asNumber()))
-      .flatten()
-      .unique()
-      .map(num => `${num}`)
-      .filter(num => {
-        for (const i of range(1, num.length-1)) {
-          const windowed = num.split("").windowed(i);
-          if (windowed.every(set => set.join() === windowed[0].join())) {
-            return true
-          }
-        }
-      })
-      .map(Number)
-      .sum()
-    return data
-  }
+  expectedPart1Results = () => [["sample.txt", 1227775554]];
+  expectedPart2Results = () => [["sample.txt", 4174379265]];
+
+  part1 = (input: string) => findInvalidNumbers(input, /^(.*)\1$/);
+  part2 = (input: string) => findInvalidNumbers(input, /^(.*)\1+$/);
 }
+
+const findInvalidNumbers = (input: string, validation: RegExp) =>
+  input
+    .matchAllAsList(/\d+\-\d+/g)
+    .map((i) => i[0])
+    .map((pair) => pair.split("-").toNums())
+    .map((pair) => range(pair[0], pair[1]))
+    .flatten()
+    .unique()
+    .map(String)
+    .filter(validation.test.bind(validation))
+    .toNums()
+    .sum();
 
 if (import.meta.main) {
   new Day02().run();
