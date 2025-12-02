1a4fae31710e04f58425ba4e13be74ace8d775c5
Grey Lovelace <austin.grey.lovelace@gmail.com>
2025-12-02 12:42:40 -0600

    refactor day01

--- src/days/day01/day01.ts
+++ src/days/day01/day01.ts
@@ -14,28 +14,32 @@ export default class Day01 extends Day {
 }
 
 const parse = (input: string, countPasses = false) => {
-  const instr = input
+  return input
     .lines()
     .map((line) => [line[0], line.slice(1)])
-    .map(([dir, amount]) => Number(amount) * (dir === "R" ? 1 : -1));
-  let times = 0;
-  let current = 50;
-  for (const amount of instr) {
-    const startedOnZero = current === 0;
-    current += amount;
-    let passes = Math.abs(Math.floor(current / 100));
-    current = current % 100;
-    if (current < 0) current += 100;
+    .map(([dir, amount]) => Number(amount) * (dir === "R" ? 1 : -1))
+    .reduce((acc, amount) => {
+      let newVal = acc.current + amount;
+      let passes = Math.abs(Math.floor(newVal / 100));
+      newVal = newVal % 100;
+      if (newVal < 0) newVal += 100;
 
-    if (current === 0) times++;
-    if (countPasses) {
-      // If we ended on zero, we also counted it as a pass, so remove one.
-      // If we started on zero, and turned left, we also counted it as a pass, so remove one.
-      if (current === 0 || (startedOnZero && amount < 0)) passes--;
-      times += Math.max(0, passes);
-    }
-  }
-  return times;
+      let timesToAdd = 0
+      if (newVal === 0) timesToAdd++;
+      if (countPasses) {
+        // If we ended on zero, we also counted it as a pass, so remove one.
+        // If we started on zero, and turned left, we also counted it as a pass, so remove one.
+        if (newVal === 0 || (acc.current === 0 && amount < 0)) passes--;
+        timesToAdd += Math.max(0, passes);
+      }
+      return {
+        current: newVal,
+        times: acc.times + timesToAdd
+      }
+    }, {
+      current: 50,
+      times: 0
+    }).times
 };
 
 if (import.meta.main) {
