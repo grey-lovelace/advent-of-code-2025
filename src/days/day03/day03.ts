5ecfb754a1fe0760d0b9919b0ba47f1e2c6329d6
Grey Lovelace <austin.grey.lovelace@gmail.com>
2025-12-03 00:40:27 -0600

    refactor Day03

--- src/days/day03/day03.ts
+++ src/days/day03/day03.ts
@@ -1,39 +1,40 @@
-import { range } from "../../utils/range.ts";
 import Day from "../day.ts";
 
 export default class Day03 extends Day {
   dayPath = () => import.meta.dirname!;
-  expectedPart1Results = () => [
-    ["sample.txt", 357]
-  ];
-  expectedPart2Results = () => [
-    ["sample.txt", 3121910778619]
-  ];
+  expectedPart1Results = () => [["sample.txt", 357]];
+  expectedPart2Results = () => [["sample.txt", 3121910778619]];
 
-  part1 = (input: string) => findMaxJoltage(input, 2)
-  part2 = (input: string) => findMaxJoltage(input, 12)
+  part1 = (input: string) => findTotal(input, 2);
+  part2 = (input: string) => findTotal(input, 12);
 }
 
-const findMaxJoltage = (input: string, digits: number) =>
+const findTotal = (input: string, digits: number) =>
   input
     .lines()
-    .map(line => line.split("").map(Number))
-    .map(nums => {
-      const highTracker = Array(digits).fill(0)
-      for (const [i, num] of nums.entries()) {
-        for (const [i2, tracker] of highTracker.entries()) {
-          if (num > tracker && i < (nums.length-(highTracker.length - 1 - i2))) {
-            highTracker[i2] = num
-            for (const overwriteI of range(i2+1, highTracker.length-1)) {
-              highTracker[overwriteI] = 0
-            }
-            break
-          }
+    .map((line) => line.split("").map(Number))
+    .map((bank) => findMaxJoltage(bank, digits))
+    .sum();
+
+const findMaxJoltage = (bank: number[], digits: number) =>
+  bank
+    .entries()
+    .reduce((acc, [batteryIndex, battery]) => {
+      for (const [trackerIndex, tracker] of acc.entries()) {
+        const isEarlyEnoughInBank =
+          batteryIndex <= bank.length - (digits - trackerIndex);
+        if (isEarlyEnoughInBank && battery > tracker) {
+          return [
+            ...acc.slice(0, trackerIndex),
+            battery,
+            ...Array(digits - trackerIndex - 1).fill(0),
+          ];
         }
       }
-      return Number(highTracker.join(""))
-    })
-    .sum()
+      return acc;
+    }, Array(digits).fill(0))
+    .join("")
+    .asNumber();
 
 if (import.meta.main) {
   new Day03().run();
