b377d8253fcc638d2103bc34e3651887f3d6d0c1
Grey Lovelace <austin.grey.lovelace@gmail.com>
2025-12-07 01:15:57 -0600

    Update template and include template input file

--- src/days/template/daytemplate.ts
+++ src/days/template/daytemplate.ts
@@ -2,18 +2,20 @@ import Day from "../day.ts";
 
 export default class DayTemplate extends Day {
   dayPath = () => import.meta.dirname!;
-  expectedPart1Results = () => [["sample.txt", 0]];
+  expectedPart1Results = () => [
+    ["sample.txt", 0]
+  ];
   expectedPart2Results = () => [
     // ["sample.txt", 0]
   ];
-  override runPart1 = true
-  override runPart2 = false
 
+  override runPart1 = true
   part1 = (input: string) => {
     const data = input
     return data
   }
 
+  override runPart2 = false
   part2 = (input: string) => {
     const data = input
     return data
