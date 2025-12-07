760324d380aa33943964913625b2c7467bf4bcc1
Grey Lovelace <austin.grey.lovelace@gmail.com>
2025-12-07 01:14:14 -0600

    day07 refactor

--- src/days/day07/day07.ts
+++ src/days/day07/day07.ts
@@ -1,4 +1,4 @@
-import { Grid, Point } from "../../utils/grid.ts";
+import { Grid } from "../../utils/grid.ts";
 import { range } from "../../utils/range.ts";
 import Day from "../day.ts";
 
@@ -7,34 +7,41 @@ export default class Day07 extends Day {
   expectedPart1Results = () => [["sample.txt", 21]];
   expectedPart2Results = () => [["sample.txt", 40]];
 
-  part1 = (input: string) => traverse(input);
-  part2 = (input: string) => traverse(input, true);
+  part1 = (input: string) => traverse(input).timesSplit;
+  part2 = (input: string) => traverse(input).timelines;
 }
 
-const traverse = (input: string, timelines = false) => {
+const traverse = (input: string) => {
   const grid = Grid.fromString(input);
   const start = grid.points.find((p) => p.val === "S")!;
-  let paths = [{ point: start, times: 1 }];
   let timesSplit = 0;
-  range(0, grid.maxY - 1).forEach(() => {
-    paths = paths
-      .map(({ point, times }) => ({ point: point.south()!, times }))
-      .flatMap(({ point, times }) =>
-        point.val == "^"
-          ? [
-              { point: point.east()!, times },
-              { point: point.west()!, times },
-            ].also(() => timesSplit++)
-          : [{ point, times }]
-      )
-      .reduce((acc, { point, times }) => {
-        const found = acc.find((item) => item.point === point);
-        if (!found) return [...acc, { point, times }];
-        found.times += times;
-        return acc;
-      }, [] as { point: Point<string>; times: number }[]);
-  });
-  return timelines ? paths.map(({ times }) => times).sum() : timesSplit;
+  return range(0, grid.maxY - 1)
+    .reduce((currentPoints) =>
+      currentPoints
+        .map(({ point, timelines }) => ({ point: point.south()!, timelines }))
+        .flatMap(({ point, timelines }) =>
+          point.val === "^"
+            ? [
+                { point: point.east()!, timelines },
+                { point: point.west()!, timelines },
+              ].also(() => timesSplit++)
+            : [{ point, timelines }]
+        )
+        .groupedBy(
+          ({ point }) => point.toString(),
+          (currentPoint) => currentPoint
+        )
+        .values()
+        .map((pointGroup) => ({
+          point: pointGroup[0].point,
+          timelines: pointGroup.map((pg) => pg.timelines).sum(),
+        })),
+      [{ point: start, timelines: 1 }]
+    )
+    .let((paths) => ({
+      timesSplit,
+      timelines: paths.map(({ timelines }) => timelines).sum(),
+    }));
 };
 
 if (import.meta.main) {
